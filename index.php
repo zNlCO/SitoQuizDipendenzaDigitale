e148b03789591ee0c993d51f8036fc7d0f224a3c
Nico <87064940+zNlCO@users.noreply.github.com>
2023-05-31 21:38:28 +0200

    Add files via upload
    file index

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ita">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Registrazione</title>
+    
+    <style>
+        h1 {text-align: center;}
+        Form{text-align: center;}
+    </style>
+    
+</head>
+
+<body>
+
+<div class="quiz-container" id="quiz">
+<div class="quiz-header">
+    <h1 style="margin-bottom:5%">Log-in</h1>
+<form action="quiz.php" method="POST" name="Form">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<div class="form__group">
+  <input type="text" class="form__input" id="name"  name="username" placeholder="Username" required="" />
+  <button class="button-64" role="button" tybe="Submit" name="Invia"><span class="text">Submit</span></button>
+                               <a href="Lavori_EdCivica_Radu.zip" download>link_Radu</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="EdPaolini.zip" download>link_Paolini</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="EdCivica_Gaetano.zip" download>link_Gaetano</a>&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+</form>
+</div>
+</div>
+
+    
+</body>
+
+</html>
